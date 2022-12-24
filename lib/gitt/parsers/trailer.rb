778d390bb5adbba1d488bb0cbf1545299e8e20fb
Brooke Kuhlmann <brooke@alchemists.io>
2022-12-24 07:34:10 -0700

    Added commit trailer parser
    Necessary to parse raw payload from Git CLI and answer a corresponding record (as implemented in the previous commit).

---
+++ lib/gitt/parsers/trailer.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Gitt
+  module Parsers
+    # Parses raw trailer data to produce a trailer record.
+    class Trailer
+      PATTERN = /
+        (?<key>\A.+)         # Key (anchored to start of line).
+        (?<delimiter>:)      # Colon delimiter.
+        (?<space>\s?)        # Space (optional).
+        (?<value>.*?)        # Value.
+        \Z                   # End of line.
+      /x
+
+      def initialize model: Models::Trailer, pattern: PATTERN
+        @pattern = pattern
+        @model = model
+      end
+
+      def call content
+        content.match(pattern)
+               .then { |data| data ? data.named_captures : {} }
+               .then { |attributes| model[attributes] }
+      end
+
+      private
+
+      attr_reader :pattern, :model
+    end
+  end
+end
---
+++ spec/lib/gitt/parsers/trailer_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Gitt::Parsers::Trailer do
+  subject(:parser) { described_class.new }
+
+  describe "#call" do
+    it "answers record for valid content" do
+      expect(parser.call("Test: abc")).to have_attributes(
+        key: "Test",
+        delimiter: ":",
+        space: " ",
+        value: "abc"
+      )
+    end
+
+    it "answers record with key and delimiter only" do
+      expect(parser.call("Test:")).to have_attributes(
+        key: "Test",
+        delimiter: ":",
+        space: "",
+        value: ""
+      )
+    end
+
+    it "answers record with no space" do
+      expect(parser.call("Test:abc")).to have_attributes(
+        key: "Test",
+        delimiter: ":",
+        space: "",
+        value: "abc"
+      )
+    end
+
+    it "answers empty record for invalid content" do
+      expect(parser.call("bogus")).to have_attributes(
+        key: nil,
+        delimiter: nil,
+        space: nil,
+        value: nil
+      )
+    end
+  end
+end
