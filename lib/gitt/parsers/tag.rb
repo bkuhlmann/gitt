68c70dbde59e9865e32bcd1ed39d96100b8463ba
Brooke Kuhlmann <brooke@alchemists.io>
2022-12-24 07:34:11 -0700

    Added tag parser
    Necessary to parse raw XML information from a Git tag command into a proper record for further use.
    
    The specs are failing with this commit because tag command does not exist yet but this issue will be fixed in the next commit.

---
+++ lib/gitt/parsers/tag.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require "refinements/hashes"
+
+module Gitt
+  module Parsers
+    # Parses raw tag information to produce a tag record.
+    class Tag
+      using Refinements::Hashes
+
+      def initialize attributer: Attributer.with(Commands::Tag::KEY_MAP.keys),
+                     sanitizers: Sanitizers::CONTAINER,
+                     model: Models::Tag
+        @attributer = attributer
+        @sanitizers = sanitizers
+        @model = model
+      end
+
+      def call content
+        attributes = attributer.call content
+        attributes.transform_with! author_email: email_sanitizer,
+                                   authored_at: date_sanitizer,
+                                   committed_at: date_sanitizer,
+                                   committer_email: email_sanitizer,
+                                   version: -> value { value.delete_prefix "refs/tags/" if value }
+        model[attributes]
+      end
+
+      private
+
+      attr_reader :attributer, :sanitizers, :model
+
+      def date_sanitizer = sanitizers.fetch(:date)
+
+      def email_sanitizer = sanitizers.fetch(:email)
+    end
+  end
+end
---
+++ spec/lib/gitt/parsers/tag_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Gitt::Parsers::Tag do
+  subject(:parser) { described_class.new }
+
+  describe "#call" do
+    it "answers record" do
+      content = <<~CONTENT
+        <author_email><test@example.com></author_email>
+        <author_name>Test User</author_name>
+        <authored_at>1668984438 -0700</authored_at>
+        <authored_relative_at>0 seconds ago</authored_relative_at>
+        <committed_at>1668984438 -0700</committed_at>
+        <committed_relative_at>0 seconds ago</committed_relative_at>
+        <committer_email><test@example.com></committer_email>
+        <committer_name>Test User</committer_name>
+        <body>This is a test.</body>
+        <sha>180dec7d8ae8cbe3565a727c63c2111e49e0b737</sha>
+        <subject>Version 0.0.0</subject>
+        <version>refs/tags/0.0.0</version>
+      CONTENT
+
+      expect(parser.call(content)).to have_attributes(
+        author_email: "test@example.com",
+        author_name: "Test User",
+        authored_at: "1668984438",
+        authored_relative_at: "0 seconds ago",
+        committer_email: "test@example.com",
+        committer_name: "Test User",
+        committed_at: "1668984438",
+        committed_relative_at: "0 seconds ago",
+        body: "This is a test.",
+        sha: "180dec7d8ae8cbe3565a727c63c2111e49e0b737",
+        subject: "Version 0.0.0",
+        version: "0.0.0"
+      )
+    end
+
+    it "strips author email of less than and greater than characters" do
+      expect(parser.call("<author_email><test@example.com></author_email>")).to have_attributes(
+        author_email: "test@example.com"
+      )
+    end
+
+    it "strips authored at of time zone" do
+      expect(parser.call("<authored_at>1668984438 -0700</authored_at>")).to have_attributes(
+        authored_at: "1668984438"
+      )
+    end
+
+    it "strips committed at of time zone" do
+      expect(parser.call("<committed_at>1668984438 -0700</committed_at>")).to have_attributes(
+        committed_at: "1668984438"
+      )
+    end
+
+    it "strips committer email of less than and greater than characters" do
+      expect(parser.call("<committer_email><a@b.com></committer_email>")).to have_attributes(
+        committer_email: "a@b.com"
+      )
+    end
+
+    it "answers strips version of tag reference path" do
+      expect(parser.call("<version>refs/tags/0.0.0</version>")).to have_attributes(
+        version: "0.0.0"
+      )
+    end
+  end
+end
