a035462e8e878971f9c69d430d9828086f002994
Brooke Kuhlmann <brooke@alchemists.io>
2023-07-19 09:46:36 -0600

    Added directable module
    Necessary to extract functionality for use in objects that behave like a commit. This will soon be used by the link:https://alchemists.io/projects/milestoner[Milestoner] gem by using a different data structure but with the same behavior as the `Commit` model in this project. Other projects might benefit from this functionality in the future too.
    
    Milestone: minor

---
+++ lib/gitt/directable.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Gitt
+  # Provides shared behavior for objects that can act like a commit.
+  module Directable
+    def directive? = amend? || fixup? || squash?
+
+    def amend? = subject.match?(/\Aamend!\s/)
+
+    def fixup? = subject.match?(/\Afixup!\s/)
+
+    def squash? = subject.match?(/\Asquash!\s/)
+
+    def prefix = subject[/\A[\w\!]+/]
+  end
+end
