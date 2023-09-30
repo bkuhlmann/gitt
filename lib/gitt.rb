d2e95d6733012f80f21b58e94081b19bf23a8505
Brooke Kuhlmann <brooke@alchemists.io>
2023-09-30 15:47:06 -0600

    Fixed Zeitwerk loader
    Necessary to ensure the gem is properly tagged and the default inflector is used since the gem inflector unfortunately looks for a version file which is not used by this gem.
    
    Milestone: patch

--- lib/gitt.rb
+++ lib/gitt.rb
@@ -2,9 +2,10 @@
 
 require "zeitwerk"
 
-Zeitwerk::Loader.for_gem.then do |loader|
+Zeitwerk::Loader.new.then do |loader|
   loader.inflector.inflect "container" => "CONTAINER"
-  loader.ignore "#{__dir__}/gitt/rspec/shared_contexts"
+  loader.tag = File.basename __FILE__, ".rb"
+  loader.push_dir __dir__
   loader.setup
 end
 
