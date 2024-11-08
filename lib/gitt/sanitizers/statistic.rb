fe7e0d6d318fa95e3a77b49a6458792a8d8df5f7
Brooke Kuhlmann <brooke@alchemists.io>
2024-11-08 15:53:25 -0700

    Updated statistic sanitzer terminology
    Necessary to clarify terminology and improve readability.
    
    Milestone: major

--- lib/gitt/sanitizers/statistic.rb
+++ lib/gitt/sanitizers/statistic.rb
@@ -12,7 +12,7 @@ module Gitt
         (?<kind>file|insertion|deletion)  # Kind capture group.
       /x
 
-      def self.update_stats attributes, kind, total
+      def self.update attributes, kind, total
         case kind
           when "file" then attributes[:files_changed] = total
           when "insertion" then attributes[:insertions] = total
@@ -21,22 +21,22 @@ module Gitt
         end
       end
 
-      def initialize empty: EMPTY, pattern: PATTERN
-        @empty = empty
+      def initialize attributes = EMPTY, pattern: PATTERN
+        @attributes = attributes
         @pattern = pattern
       end
 
       def call text
-        return empty unless text
+        return attributes unless text
 
-        text.scan(pattern).each.with_object(empty.dup) do |(number, kind), aggregate|
-          self.class.update_stats aggregate, kind, number.to_i
+        text.scan(pattern).each.with_object(attributes.dup) do |(number, kind), aggregate|
+          self.class.update aggregate, kind, number.to_i
         end
       end
 
       private
 
-      attr_reader :empty, :pattern
+      attr_reader :attributes, :pattern
     end
   end
 end
