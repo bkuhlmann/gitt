be9e06029cf617f82f4cc6a8177b4c18af8fcb1a
Brooke Kuhlmann <brooke@alchemists.io>
2025-05-21 14:32:17 -0600

    Fixed Zeitwerk 2.7.3 loader failure
    Necessary to address exceptions and spec failures with the recent release of Zeitwerk link:https://github.com/fxn/zeitwerk/blob/main/CHANGELOG.md#273-20-may-2025[2.7.3]. Zeitwerk introduced a breaking change which shouldn't have been a patch release because the `#find` method is no longer available. We have to use `#each` first.
    
    Milestone: patch

--- lib/gitt.rb
+++ lib/gitt.rb
@@ -15,7 +15,7 @@ module Gitt
   SHELL = Shell.new.freeze
 
   def self.loader registry = Zeitwerk::Registry
-    @loader ||= registry.loaders.find { |loader| loader.tag == File.basename(__FILE__, ".rb") }
+    @loader ||= registry.loaders.each.find { |loader| loader.tag == File.basename(__FILE__, ".rb") }
   end
 
   def self.new(...) = Repository.new(...)
